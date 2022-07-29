2a70caee8c140f408ef3f1faed88b0a746d9533c
Mustafa J <mustafa.0x@gmail.com>
2022-07-29 18:09:12 +0300

    codemirror: cleanups

--- src/CodeMirror.svelte
+++ src/CodeMirror.svelte
@@ -1,24 +1,23 @@
 <div class=cm-wrapper use:add_to_dom style="--max_height: {max_height}px"></div>
 
 <script>
-import {keymap} from '@codemirror/view';
-import {EditorView, basicSetup} from 'codemirror';
-import {javascript} from '@codemirror/lang-javascript';
-import {indentWithTab} from '@codemirror/commands';
-import {indentOnInput, indentUnit} from '@codemirror/language';
+import {EditorView, basicSetup} from 'codemirror'
+import {keymap} from '@codemirror/view'
+import {javascript} from '@codemirror/lang-javascript'
+import {indentWithTab} from '@codemirror/commands'
+import {indentUnit} from '@codemirror/language'
 
-const extensions = [
-    basicSetup,
-    indentUnit.of('    '),
-    keymap.of([indentWithTab]),
-    javascript(),
-    EditorView.updateListener.of(v => {
-        if (v.docChanged)
-            dispatch('change', v);
-    }),
-];
 const editor = new EditorView({
-    extensions,
+    extensions: [
+        basicSetup,
+        indentUnit.of('    '),
+        keymap.of([indentWithTab]),
+        javascript(),
+        EditorView.updateListener.of(v => {
+            if (v.docChanged)
+                dispatch('change', v)
+        }),
+    ],
 });
 
 export const get_value = () => editor.state.doc.toString();
